e55b5e4aa560951d7859bf5b80dd0c69fa535a1e
abdul2801 <afdul100ahmad@gmail.com>
2024-02-10 23:17:21 +0530

    changes to challenges

--- src/Pages/PastChallenges.tsx
+++ src/Pages/PastChallenges.tsx
@@ -1,7 +1,87 @@
-import React from 'react'
-
+import React from "react";
+import "./PastChallenges.css";
 export default function PastChallenges() {
   return (
-    <div className='container'>PastChallenges</div>
-  )
+    <div className="past_cont">
+      <h1 className="active">Challenges</h1>
+
+      <div className="">
+        <div>
+          <h2 className="genre_heading active">Genre</h2>
+          <div className="challenges_cont">
+            <div className="a-courses_item">
+              <a href="#" className="a-courses-item_link">
+                <div className="a-courses-item_bg"></div>
+
+                <div className="a-courses-item_title">
+                  Challenge Title lorem20
+                </div>
+
+                <div className="a-courses-item_date-box">
+                  
+                  <span className="a-courses-item_date">Tiny Description</span>
+                </div>
+              </a>
+            </div>
+            <div className="a-courses_item">
+              <a href="#" className="a-courses-item_link">
+                <div className="a-courses-item_bg"></div>
+
+                <div className="a-courses-item_title">
+                  Challenge Title lorem20
+                </div>
+
+                <div className="a-courses-item_date-box">
+                  
+                  <span className="a-courses-item_date">Tiny Description</span>
+                </div>
+              </a>
+            </div>
+            <div className="a-courses_item">
+              <a href="#" className="a-courses-item_link">
+                <div className="a-courses-item_bg"></div>
+
+                <div className="a-courses-item_title">
+                  Challenge Title lorem20
+                </div>
+
+                <div className="a-courses-item_date-box">
+                  
+                  <span className="a-courses-item_date">Tiny Description</span>
+                </div>
+              </a>
+            </div>
+            <div className="a-courses_item">
+              <a href="#" className="a-courses-item_link">
+                <div className="a-courses-item_bg"></div>
+
+                <div className="a-courses-item_title">
+                  Challenge Title lorem20
+                </div>
+
+                <div className="a-courses-item_date-box">
+                  
+                  <span className="a-courses-item_date">Tiny Description</span>
+                </div>
+              </a>
+            </div>
+            <div className="a-courses_item">
+              <a href="#" className="a-courses-item_link">
+                <div className="a-courses-item_bg"></div>
+
+                <div className="a-courses-item_title">
+                  Challenge Title lorem20
+                </div>
+
+                <div className="a-courses-item_date-box">
+                  
+                  <span className="a-courses-item_date">Tiny Description</span>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
